cfe9f5002aa984c8279d0913efb9df88d3b70c63
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-11-27 23:58:22 -0800

    Merge sort complete

--- mergesort.js
+++ mergesort.js
@@ -1,17 +1,30 @@
 //Build a function mergeSort that takes in an array and returns a sorted array, using a recursive merge sort methodology.
-let sortArray = [2, 6, 1, 9, 4, 3, 5, 7];
+let sortArray = [2, 6, 1, 9, 4, 3, 5, 7, 8];
 
 function mergeSort(array) {
     //Split the given array into 2 halfs
     if (array.length == 1) {
-        return "Array length is 1"
+        return array;
     } else {
-        let halfLength = array.length / 2;
-        let leftArray = array.slice(0, halfLength);
-        let rightArray = array.slice(halfLength, halfLength * 2)
-        console.log(leftArray);
-        console.log(rightArray);
-        return mergeSort(leftArray) && mergeSort(rightArray);
-    }
+        let middle = array.length / 2;
+        let leftArray = array.slice(0, middle);
+        let rightArray = array.slice(middle, middle * 2);
 
+        let sortedLeftArray = mergeSort(leftArray);
+        let sortedRightArray = mergeSort(rightArray);
+
+        return merge(sortedLeftArray, sortedRightArray);
+    }
+}
+
+function merge(leftArray, rightArray) {
+    let newArray = [];
+    while (leftArray.length && rightArray.length) {
+        if (leftArray[0] < rightArray[0]) {
+            newArray.push(leftArray.shift());
+        } else {
+            newArray.push(rightArray.shift());
+        }
+    }
+    return newArray.concat(leftArray.slice().concat(rightArray.slice()));
 }
